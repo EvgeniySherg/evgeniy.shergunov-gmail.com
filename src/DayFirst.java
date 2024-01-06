3fb18904dace24f2b40ad0c88fd11ff9e2956150
EvgeniyS <shergunov.evgeniy@gmail.com>
2024-01-06 12:48:34 +0300

    The tasks of the first day are completed.

---
+++ src/DayFirst.java
@@ -0,0 +1,46 @@
+public class DayFirst {
+    public static void main(String[] args) {
+        System.out.print("First homework");
+
+
+        System.out.println("First task - while \n");
+        int count = 0;
+        while (count < 10) {
+            System.out.print("Java ");
+            count++;
+        }
+        System.out.println("\n__________________________");
+
+        System.out.println("Second task - for \n");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Java " );
+        }
+        System.out.println("\n__________________________");
+
+        System.out.print("Third task - for or while");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Java " );
+        }
+        System.out.println("__________________________");
+
+        System.out.println("Four task - olimpic games \n");
+        int countYear = 1980;
+        while (countYear <= 2020) {
+            System.out.printf("Олимпиада - %d года \n", countYear);
+            countYear += 4;
+        }
+        System.out.println("__________________________");
+
+        System.out.print("Fifth task - olimpic games with for");
+        for (int i = 1980; i <= 2020; i+=4) {
+            System.out.printf("Олимпиада - %d года \n", i);
+        }
+        System.out.println("__________________________");
+
+        System.out.print("Six task - multiplication table");
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("5 x %d = %d\n", i, i*5);
+        }
+        System.out.println("__________________________");
+    }
+}
